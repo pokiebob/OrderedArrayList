b73be1e5837e8d94c96818934c0f4b811e9df5d7
pokiebob <ccursetjee@gmail.com>
2020-12-13 20:54:41 -0500

    constructors

---
+++ NoNullArrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class NoNullArrayList<T> extends ArrayList<T> {
+
+	NoNullArrayList() {
+		super();
+	}
+
+	NoNullArrayList(int startingCapacity) {
+		super(startingCapacity);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+
+
+}
