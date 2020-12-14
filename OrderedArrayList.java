59bb6c02cbfe413dbff5474aab19158d0016b9cf
pokiebob <ccursetjee@gmail.com>
2020-12-13 21:05:38 -0500

    constructor methods

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,6 +1,13 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
 
-	
+	OrderedArrayList() {
+		super();
+	}
+
+	OrderedArrayList(int startingCapacity) {
+		super(startingCapacity);
+	}
+
 
 	public static void main(String[] args) {
 
