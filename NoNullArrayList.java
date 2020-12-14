22b824f23b2642437a7d71d2c28b2aa5325232c3
pokiebob <ccursetjee@gmail.com>
2020-12-13 20:55:12 -0500

    add methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -10,6 +10,22 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 		super(startingCapacity);
 	}
 
+	public void add(T element) {
+		if (element == null) {
+			throw new IllegalArgumentException("element " + element + "cannot be null");
+		} else {
+			super.add(element);
+		}
+	}
+
+	public void add(int index, T element) {
+		if (element == null) {
+			throw new IllegalArgumentException("element " + element + "cannot be null");
+		} else {
+			super.add(index, element);
+		}
+	}
+
 	public static void main(String[] args) {
 
 	}
